64f4e7108c4164b7f0d0fc037cbdf2d8c6f7502a
Jai Shukla <iamshuklajai@gmail.com>
2023-07-12 22:46:58 +0530

    sign out and forgot password added
    sign out is tested and working successfully forgot password will be checked after backend implementation

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -31,15 +31,11 @@ export function checkUser(loginInfo) {
   });
 }
 
-export function updateUser(update) {
+export function signOut(userId) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/users/'+update.id, {
-      method: 'PATCH',
-      body: JSON.stringify(update),
-      headers: { 'content-type': 'application/json' },
-    });
-    const data = await response.json();
-    // TODO: on server it will only return some info of user (not password)
-    resolve({ data });
+    // TODO: on server we will remove user session info
+    resolve({ data: 'success' });
   });
 }
+
+
