cb2568893c24d4f24339490a7bb80efa8ddc8a95
Jai Shukla <iamshuklajai@gmail.com>
2023-07-14 02:39:21 +0530

    admin order and order status added

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,3 +1,7 @@
 export const ITEMS_PER_PAGE = 10;
+export function discountedPrice(item){
+    return Math.round(item.price*(1-item.discountPercentage/100),2)
+}
+
 //json-server --watch data.json --port 8080 
 //to start the json server
